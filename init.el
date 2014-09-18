8689a2444002deabbf80bee8744296a0b4b60fb7
wangyu_air <wangyu0042@gmail.com>
2014-09-18 23:22:48 +0800

    Add f.el and s.el.

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/inits")
 
+(require 'init-modern-api)
+
 (require 'init-configs)
 
 (if (file-exists-p "~/.emacs.d/personal.el")
---
+++ inits/init-modern-api.el
@@ -0,0 +1,10 @@
+(add-to-list 'load-path "~/.emacs.d/packages/dash")
+(require 'dash)
+
+(add-to-list 'load-path "~/.emacs.d/packages/s")
+(require 's)
+
+(add-to-list 'load-path "~/.emacs.d/packages/f")
+(require 'f)
+
+(provide 'init-modern-api)
