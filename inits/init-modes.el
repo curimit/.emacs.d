8cc19e1a684019bbbaf83930a9c65f11cd860d54
curimit <curimit@gmail.com>
2014-09-28 23:27:51 +0800

    Move yasnippet to userdata dir.

--- inits/init-modes.el
+++ inits/init-modes.el
@@ -68,7 +68,7 @@
 (add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
 (require 'yasnippet)
 (setq yas-snippet-dirs
-      '("~/.emacs.d/packages/yasnippet/snippets"         ;; the default collection
+      '("~/.emacs.d/userdata/snippets"
         ))
 (yas-global-mode 1)
 
