2ccdfc6bc9abb99c9f3690edb4357eea2cdf20dd
curimit <curimit@gmail.com>
2014-10-06 12:40:34 +0800

    Fix error: forgot add tern's load-path.

--- inits/init-modes.el
+++ inits/init-modes.el
@@ -203,7 +203,7 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 ;; tern
-
+(add-to-list 'load-path "~/.emacs.d/packages/tern/emacs")
 (autoload 'tern-mode "tern.el" nil t)
 
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
