9e4632e740ec998e5d23288634c58a1c847d317a
curimit <curimit@gmail.com>
2014-10-06 15:48:14 +0800

    Implement macro `linq`.

--- inits/init-modern-api.el
+++ inits/init-modern-api.el
@@ -7,4 +7,39 @@
 (add-to-list 'load-path "~/.emacs.d/packages/f")
 (require 'f)
 
+(defmacro linq (result &rest functions)
+  "only accept alpha"
+  (let ((f nil) (args nil))
+    (loop for item in functions do
+          ;; check if it is a function
+          (if (and (listp item) (equal (car item) 'quote))
+              (progn
+                (if (not (equal f nil))
+                    (progn
+                      (if (equal args nil)
+                          (setq result (-concat (list f) (list result)))
+                        (setq result (-concat (list f) args (list result)))
+                        )
+                      )
+                  )
+                (setq f (car (cdr item)))
+                (setq args nil)
+                )
+            (progn
+              (setq args (-concat args (list item)))
+              )
+            )
+          )
+    (if (not (equal f nil))
+        (progn
+          (if (equal args nil)
+              (setq result (-concat (list f) (list result)))
+            (setq result (-concat (list f) args (list result)))
+            )
+          )
+      )
+    )
+  result
+  )
+
 (provide 'init-modern-api)
