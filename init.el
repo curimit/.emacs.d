dd7b84c659f7fc4e852b6b6508dbbf6d43b97dbd
curimit <curimit@gmail.com>
2014-10-28 15:44:38 +0800

    Add support of `preconfig.el`.

--- init.el
+++ init.el
@@ -5,7 +5,11 @@
 ;; Add all of the directories in packages to load-path
 (--map (add-to-list 'load-path it)
        (f-directories "~/.emacs.d/packages"))
-      
+
+(if (file-exists-p "~/.emacs.d/preconfig.el")
+    (load-file "~/.emacs.d/preconfig.el")
+    )
+
 (require 'init-configs)
 
 (if (file-exists-p "~/.emacs.d/personal.el")
