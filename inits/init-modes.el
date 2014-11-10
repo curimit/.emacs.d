eaa187bd4edac2bb7f3931e34f5da2b683103de4
curimit <curimit@gmail.com>
2014-11-10 14:47:59 +0800

    Add aggressive-indent to keep code aligned.

--- inits/init-modes.el
+++ inits/init-modes.el
@@ -596,4 +596,9 @@
   (add-hook hook 'hideshowvis-enable))
 (hideshowvis-symbols)
 
+;; aggressive-indent-mode
+(require 'aggressive-indent)
+(global-aggressive-indent-mode 1)
+(add-to-list 'aggressive-indent-excluded-modes 'html-mode)
+
 (provide 'init-modes)
