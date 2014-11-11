ed48f4eee1f8d43595c35c0ec201138dac6d9f29
curimit <curimit@gmail.com>
2014-11-11 10:56:52 +0800

    Add markdown mode.

--- inits/init-modes.el
+++ inits/init-modes.el
@@ -603,4 +603,11 @@
 (global-aggressive-indent-mode 1)
 (add-to-list 'aggressive-indent-excluded-modes 'html-mode)
 
+;; markdown-mode
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 (provide 'init-modes)
