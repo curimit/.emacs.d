1ef92287962c2d18de32ab8c929fc14cac34e6e0
curimit <curimit@gmail.com>
2014-12-27 22:57:25 +0800

    Modify jade keywords for polymer.

--- inits/init-modes.el
+++ inits/init-modes.el
@@ -429,14 +429,28 @@
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 (after-load "jade-mode"
+  (setq jade-keywords
+    (regexp-opt
+       '("if" "else" "for" "in" "each" "case" "when" "default" "block" "extends"
+         "name" "value" "mode" "target" "repeat" "selected" "style" "is"
+         "one" "two" "three"
+         "block append" "block prepend" "append" "prepend"
+         "fullbleed" "unresolved" "layout" "reverse"
+         "flex" "auto-vertical" "vertical" "auto-horizontal" "horizontal"
+         "start-justified" "center-justified" "end-justified" "justified"
+         "block" "hidden" "relative" "fit"
+         "self-start" "self-center" "self-end" "self-stretch"
+         "include" "yield" "mixin") 'words))
+
   (setq jade-font-lock-keywords
-  `((,"!!!\\|doctype\\( ?[A-Za-z0-9\-\_]*\\)?" 0 font-lock-comment-face) ;; doctype
-    (,jade-keywords . font-lock-keyword-face) ;; keywords
-    (,"#\\(\\w\\|_\\|-\\)*" . font-lock-variable-name-face) ;; id
-    (,"\\(?:^[ {2,}]*\\)\\(\\+[A-Za-z0-9\-\_]*\\)" 1 font-lock-warning-face) ;; mixin
-    (,"\\(?:^[ {2,}]*\\(?:[a-z0-9_:\\-]*\\)\\)?\\(#[A-Za-z0-9\-\_]*[^ ]\\)" 1 font-lock-variable-name-face) ;; id
-    (,"\\(?:^[ {2,}]*\\(?:[a-z0-9_:\\-]*\\)\\)?\\(\\.[A-Za-z0-9\-\_]*\\)" 1 font-lock-type-face) ;; class name
-    (,"^[ {2,}]*[a-z0-9_:\\-]*" 0 font-lock-function-name-face)))
+        `((,"!!!\\|doctype\\( ?[A-Za-z0-9\-\_]*\\)?" 0 font-lock-comment-face) ;; doctype
+          (,jade-keywords . font-lock-keyword-face) ;; keywords
+          (,"\\(on\\(\\-\\w+\\)+\\)" . font-lock-keyword-face) ;; on-*
+          (,"#\\(\\w\\|_\\|-\\)*" . font-lock-variable-name-face) ;; id
+          (,"^\\(?:[ {2,}]*\\)\\(\\+[A-Za-z0-9\-\_]*\\)" 1 font-lock-warning-face) ;; mixin
+          (,"^\\(?:[ {2,}]*\\(?:[a-z0-9_:\\-]*\\)\\)?\\(#[A-Za-z0-9\-\_]*[^ ]\\)" 1 font-lock-variable-name-face) ;; id
+          (,"^\\(?:[ {2,}]*\\(?:[a-z0-9_:\\-]*\\)\\)?\\(\\.[A-Za-z0-9\-\_]*\\)" 1 font-lock-type-face) ;; class name
+          (,"^[ {2,}]*[a-z0-9_:\\-]*" 0 font-lock-function-name-face)))
   )
 
 ;; function args
