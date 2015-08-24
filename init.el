74527d0586ed8ee93c1c8fd7e0679ce05d9d5156
curimit <curimit@gmail.com>
2015-08-24 19:12:15 +0800

    Don't load preconfig.el and personal.el when terminal

--- init.el
+++ init.el
@@ -6,16 +6,17 @@
 (--map (add-to-list 'load-path it)
        (f-directories "~/.emacs.d/packages"))
 
-(if (file-exists-p "~/.emacs.d/preconfig.el")
-    (load-file "~/.emacs.d/preconfig.el")
-  )
-
 (if (eq window-system nil)
     (require 'init-configs-lite)
-  (require 'init-configs)
-  )
+  (progn
+    (if (file-exists-p "~/.emacs.d/preconfig.el")
+        (load-file "~/.emacs.d/preconfig.el")
+      )
 
+    (require 'init-configs)
 
-(if (file-exists-p "~/.emacs.d/personal.el")
-    (load-file "~/.emacs.d/personal.el")
+    (if (file-exists-p "~/.emacs.d/personal.el")
+        (load-file "~/.emacs.d/personal.el")
+      )
+    )
   )
